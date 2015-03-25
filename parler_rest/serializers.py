90e32bfe5ffe72c8bb622a43b2d0bfd73e718e9f
AdrianLC <adrianlopezcalvo@gmail.com>
2015-03-25 21:20:25 +0100

    Fix automatic saving of translations

--- parler_rest/serializers.py
+++ parler_rest/serializers.py
@@ -14,23 +14,24 @@ class TranslatableModelSerializer(serializers.ModelSerializer):
 
     def save(self, **kwargs):
         """Extract the translations, store these into the django-parler model data."""
-        self._parler_translations = self._pop_translated_data()
-
+        translated_data = self._pop_translated_data()
         instance = super(TranslatableModelSerializer, self).save(**kwargs)
-
-        # TODO: Save translations
-        # translations = obj._related_data.pop(meta.rel_name, {})
-        #     if translations:
-        #         for lang_code, model_fields in translations.iteritems():
-        #             translations = obj._get_translated_model(lang_code, auto_create=True, meta=meta)
-        #             for field, value in model_fields.iteritems():
-        #                 setattr(translations, field, value)
+        self.save_translations(instance, translated_data)
         return instance
 
-    def _pop_translated_data(self, **kwargs):
-        parler_translations = {}
+    def _pop_translated_data(self):
+        translated_data = {}
         for meta in self.Meta.model._parler_meta:
-            translations = kwargs.pop(meta.rel_name, {})
+            translations = self.validated_data.pop(meta.rel_name, {})
             if translations:
-                parler_translations[meta.rel_name] = translations
-        return parler_translations
+                translated_data[meta.rel_name] = translations
+        return translated_data
+
+    def save_translations(self, instance, translated_data):
+        for meta in self.Meta.model._parler_meta:
+            translations = translated_data.get(meta.rel_name, {})
+            for lang_code, model_fields in translations.items():
+                translation = instance._get_translated_model(lang_code, auto_create=True, meta=meta)
+                for field, value in model_fields.items():
+                    setattr(translation, field, value)
+                translation.save()
