4f2d31e9f3aebd8ada3cc366f74a0863e9b4ad2a
Aarni Koskela <akx@iki.fi>
2015-06-16 11:26:39 +0300

    Add new tests.

--- testproj/serializers.py
+++ testproj/serializers.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from parler_rest.serializers import TranslatableModelSerializer, TranslatedFieldsField
 
 from .models import Country
+from rest_framework.serializers import ModelSerializer
 
 
 class CountryTranslatedSerializer(TranslatableModelSerializer):
@@ -12,3 +13,25 @@ class CountryTranslatedSerializer(TranslatableModelSerializer):
     class Meta:
         model = Country
         fields = ('pk', 'country_code', 'translations')
+
+
+class AutoSharedModelCountryTranslatedSerializer(TranslatableModelSerializer):
+    translations = TranslatedFieldsField()
+
+    class Meta:
+        model = Country
+        fields = ('pk', 'country_code', 'translations')
+
+
+class CountryTranslatedFieldsSerializer(ModelSerializer):
+    class Meta:
+        model = Country._parler_meta.root_model
+        fields = ("name",)  # Skip url for the hell of it
+
+
+class ExplicitSerializerCountryTranslatedSerializer(TranslatableModelSerializer):
+    xl = TranslatedFieldsField(serializer_class=CountryTranslatedFieldsSerializer, source="translations")
+
+    class Meta:
+        model = Country
+        fields = ('pk', 'country_code', 'xl')
